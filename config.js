bfbb987e5e94b231a62002fa010a5da0603ed841
DEW-NEW <newgithub8@gmail.com>
2025-03-04 01:33:17 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "tZBzXSKY#RcpNZ-6lSHfJv_LDFn7uaX93LXo9Qs6uMZWsC7rP6qo",
+MONGODB: process.env.MONGODB || "mongodb://mongo:HtZTZEYJvqfbOwHfTarmahlqKWzIxteZ@autorack.proxy.rlwy.net:31989",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://bit.ly/3XrPwx0",
+AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
+AUTO_BIO:"true",
+AUTO_VOICE:"true"
+};
